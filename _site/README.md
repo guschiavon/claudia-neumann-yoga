1348e5b2238b852e1028b00aba2751860dc71f3b
Gus <gustavo.schiavon@gmail.com>
2020-03-27 20:42:46 +0800

    MQ & Styles

--- _site/README.md
+++ _site/README.md
@@ -3,3 +3,4 @@
 ### Graphics attributions
 
 - <a href="https://www.freepik.com/free-photos-vectors/watercolor">Watercolor vector created by 0melapics - www.freepik.com</a>
+- <a href="https://www.freepik.com/free-photos-vectors/frame">Frame vector created by freepik - www.freepik.com</a>
--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,57 +1,47 @@
-var clics = 0;
 
-$(document).ready(function() {
-
-
-// Accordion script by @SebaGarcia
-
-  $('.faq-a').hide();
-  $('#cerrartodas').hide();
-
-  $('.faq-q').click(function() {
-
-    $(this).next('.faq-a').toggle(function() {
-
-      $(this).next('.faq-a');
-
-    }, function() {
-
-      $(this).next('.faq-a').fadeIn('fast');
-
-    });
-
-    if ($(this).hasClass('close')) {
-      $(this).removeClass('close');
-    } else {
-      $(this).addClass('close');
-    };
-
-    if ($('.close').length >= 3) {
-
-      $('#cerrartodas').fadeIn('fast');
-
-    } else {
-      $('#cerrartodas').hide();
-      var opened = $('.close').length
-      console.log(opened);
-    }
-  }); //Close Function Click
-
-}); //Close Function Ready
-
-$('#cerrartodas').click(function() {
-  $('.faq-a').fadeOut(200);
-  $('.faq-q').removeClass('close');
-  $('#cerrartodas').fadeOut('fast');
-});
-
-
+// var clics = 0;
+//
 // $(document).ready(function() {
 //
-//   console.log('Hello from jQuery');
+//   // $('.burger-menu').click(function() {
+//   // $('.mobile-nav').show();
+//   //
 //
-//   $(".faq").click(function(){
-//       $(".faq-accordion").toggle(200);
+//
+//
+// // Accordion script by @SebaGarcia
+//
+//   $('.faq-a').hide();
+//   $('#cerrartodas').hide();
+//
+//   $('.faq-q').click(function() {
+//
+//     $(this).next('.faq-a').toggle(function() {
+//
+//       $(this).next('.faq-a');
+//
+//     }, function() {
+//
+//       $(this).next('.faq-a').fadeIn('fast');
+//
 //     });
 //
-// });
+//     if ($(this).hasClass('close')) {
+//       $(this).removeClass('close');
+//     } else {
+//       $(this).addClass('close');
+//     };
+//
+//     if ($('.close').length >= 3) {
+//
+//       $('#cerrartodas').fadeIn('fast');
+//
+//     } else {
+//       $('#cerrartodas').hide();
+//       var opened = $('.close').length
+//       console.log(opened);
+//     }
+//   }); //Close Function Click
+//
+// // }); //Close Function Click
+// }); //Close Function Ready
