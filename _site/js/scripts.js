71c00e88c9fa4e29c8468bd1ceda997807371bde
Gus <gustavo.schiavon@gmail.com>
2020-04-21 10:51:04 +0800

    Implement lazysizes for responsive images

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -15,46 +15,6 @@ $(document).ready(function() {
 
 }); //Closes Ready function
 
-var clics = 0;
-
-$(document).ready(function() {
-
-// Accordion script by @SebaGarcia
-console.log('FAQ loaded');
-
-  $('.faq-a').hide();
-  $('#cerrartodas').hide();
-
-  $('.faq-q').click(function() {
-
-    $(this).next('.faq-a').toggle(function() {
-
-      $(this).next('.faq-a');
-
-    }, function() {
-
-      $(this).next('.faq-a').fadeIn('fast');
-
-    });
-
-    if ($(this).hasClass('close')) {
-      $(this).removeClass('close');
-    } else {
-      $(this).addClass('close');
-    };
-
-    if ($('.close').length >= 3) {
-
-      $('#cerrartodas').fadeIn('fast');
-
-    } else {
-      $('#cerrartodas').hide();
-      var opened = $('.close').length
-      console.log(opened);
-    }
-  }); //Close Function Click
-
-}); //Close Function Ready
 
 // Defer iFrame loading
 console.log('loaded defer iframe');
---
+++ js/faq-accordion.js
@@ -0,0 +1,40 @@
+var clics = 0;
+
+$(document).ready(function() {
+
+// Accordion script by @SebaGarcia
+console.log('FAQ loaded');
+
+  $('.faq-a').hide();
+  $('#cerrartodas').hide();
+
+  $('.faq-q').click(function() {
+
+    $(this).next('.faq-a').toggle(function() {
+
+      $(this).next('.faq-a');
+
+    }, function() {
+
+      $(this).next('.faq-a').fadeIn('fast');
+
+    });
+
+    if ($(this).hasClass('close')) {
+      $(this).removeClass('close');
+    } else {
+      $(this).addClass('close');
+    };
+
+    if ($('.close').length >= 3) {
+
+      $('#cerrartodas').fadeIn('fast');
+
+    } else {
+      $('#cerrartodas').hide();
+      var opened = $('.close').length
+      console.log(opened);
+    }
+  }); //Close Function Click
+
+}); //Close Function Ready
